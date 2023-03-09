9e51fd5b77dd450989a557fa4554a383d17da8b4
Ernest Myrmyr <ernuk@MacBook-Air.local>
2023-03-09 17:47:07 +0200

    added all effects on the first page. Ready to do everything else!

--- client/src/components/Welcome/WelcomePage.tsx
+++ client/src/components/Welcome/WelcomePage.tsx
@@ -1,15 +1,36 @@
 import css from './WelcomePage.module.css';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import { useEffect, useState } from 'react';
+import TrackVisibilty from 'react-on-screen';
 
 const WelcomePage = () => {
-  const onGetClick = () => {};
+  const [showFirst, setShowFirst] = useState(false);
+  const [showSecond, setShowSecond] = useState(false);
+  const [buttonTrigger, setButtonTrigger] = useState(false);
+
+  useEffect(() => {
+    setShowFirst(true);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => setShowSecond(true), 1700);
+    setTimeout(() => setButtonTrigger(true), 2300);
+  }, [showFirst])
 
   return (
-    <header className={css.main_div}>
-      <h2>Run out of movies to watch?</h2>
-      <h1>You're in the right place!</h1>
+    <TrackVisibilty>
+      {({ isVisible }) =>
+        <div className={css.main_div}>
+          <h2 className={showFirst && isVisible ? `animate__animated animate__fadeInDownBig ${css.main_div_first_caption}` : `${css.main_div_first_caption} ${css.dnone}`}>Run out of movies to watch?</h2>
+          <h1 className={showSecond && isVisible ? `animate__animated animate__zoomIn ${css.main_div_caption}` : `${css.main_div_caption} ${css.dnone}`}>You're in the right place!</h1>
 
-      <button onClick={onGetClick}>Get Recommendations</button>
-    </header>
+          <button className={buttonTrigger && isVisible ? `${css.button} animate__animated animate__fadeIn` : `${css.button} ${css.dnone}`}>
+            Get Recommendations <ArrowRightCircle size={25}/>
+          </button>
+        </div>
+      }
+    </TrackVisibilty>
   );
 };
 
