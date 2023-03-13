b24198e670af3e9810d98f16366b32502a25d364
Ernest Myrmyr <ernuk@MacBook-Air.local>
2023-03-13 14:56:04 +0200

    added age page. Ready to apply rest api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import css from './App.module.css';
+import Age from './components/Age/Age';
 import WelcomePage from './components/Welcome/WelcomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className={css.main_div}>
-      <WelcomePage />
+      <Router>
+        <Routes>
+          <Route path="/age" element={ <Age /> } />
+          <Route path="/" element={ <WelcomePage /> } />
+          <Route path='/*' element={ <WelcomePage /> } />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- client/src/components/Welcome/WelcomePage.tsx
+++ client/src/components/Welcome/WelcomePage.tsx
@@ -2,12 +2,13 @@ import css from './WelcomePage.module.css';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import { useEffect, useState } from 'react';
-import TrackVisibilty from 'react-on-screen';
+import { useNavigate, generatePath } from "react-router-dom";
 
 const WelcomePage = () => {
   const [showFirst, setShowFirst] = useState(false);
   const [showSecond, setShowSecond] = useState(false);
   const [buttonTrigger, setButtonTrigger] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setShowFirst(true);
@@ -18,19 +19,22 @@ const WelcomePage = () => {
     setTimeout(() => setButtonTrigger(true), 2300);
   }, [showFirst])
 
-  return (
-    <TrackVisibilty>
-      {({ isVisible }) =>
-        <div className={css.main_div}>
-          <h2 className={showFirst && isVisible ? `animate__animated animate__fadeInDownBig ${css.main_div_first_caption}` : `${css.main_div_first_caption} ${css.dnone}`}>Run out of movies to watch?</h2>
-          <h1 className={showSecond && isVisible ? `animate__animated animate__zoomIn ${css.main_div_caption}` : `${css.main_div_caption} ${css.dnone}`}>You're in the right place!</h1>
+  const onBtnClick = () => {
+    navigate(generatePath('/age'));
+  };
 
-          <button className={buttonTrigger && isVisible ? `${css.button} animate__animated animate__fadeIn` : `${css.button} ${css.dnone}`}>
-            Get Recommendations <ArrowRightCircle size={25}/>
-          </button>
-        </div>
-      }
-    </TrackVisibilty>
+  return (
+    <div className={css.main_div}>
+      <h2 className={showFirst ? `animate__animated animate__fadeInDownBig ${css.main_div_first_caption}` : `${css.main_div_first_caption} ${css.dnone}`}>Run out of movies to watch?</h2>
+      <h1 className={showSecond ? `animate__animated animate__zoomIn ${css.main_div_caption}` : `${css.main_div_caption} ${css.dnone}`}>You're in the right place!</h1>
+
+      <button
+        onClick={onBtnClick}
+        className={buttonTrigger ? `${css.button} animate__animated animate__fadeIn` : `${css.button} ${css.dnone}`}
+      >
+        Get Recommendations <ArrowRightCircle size={25}/>
+      </button>
+    </div>
   );
 };
 
